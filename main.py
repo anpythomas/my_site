c1cc62a4cdcf501cd041625eb8010251bb333921
bradleythomas <andythomas.py@gmail.com>
2022-09-02 22:54:51 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+import os
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+if os.environ['secret_key']:
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    if __name__ == '__main__':
+        app.run(debug=True)
