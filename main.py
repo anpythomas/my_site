6025d41bc4020d25b030a3bf66ebef299a528284
bradleythomas <andythomas.py@gmail.com>
2022-09-03 12:51:56 -0500

    Remove .gitignore, env vars and req.txt

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import os
 app = Flask(__name__)
 Bootstrap(app)
 
-if os.environ['secret_key']:
 
-    @app.route("/")
-    def home():
-        return render_template("index.html")
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-    if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
