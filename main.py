c3a45e1cd2ffb242fc45cfacef0f26716a3acf73
bradleythomas <andythomas.py@gmail.com>
2022-09-06 16:31:59 -0500

    Add all library's to rqts.txt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template
 import flask_bootstrap
-import os
-
-port = int(os.environ.get('PORT', 5000))
 
 
 app = Flask(__name__)
@@ -14,5 +11,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
 
