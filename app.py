994974a8f3dace96ee4935211903849a630cb827
bradleythomas <andythomas.py@gmail.com>
2022-09-16 17:41:19 -0500

    Contact tab separate page

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/<>")
-def contact():
-    return render_template("contact.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
