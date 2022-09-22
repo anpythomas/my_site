091eb481de57d524810d0b304ce6da8b9319155c
bradleythomas <andythomas.py@gmail.com>
2022-09-22 12:22:55 -0500

    Optimize padding about section p on tablet and mobile

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ Bootstrap(app)
 def home():
     return render_template("index.html")
 
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
+#
+# @app.route("/about")
+# def about():
+#     return render_template("about.html")
 
 
 if __name__ == '__main__':
