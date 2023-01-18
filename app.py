9b02fa3486bac6a95efeb3eeca839f6a1dc9adef
Andy <andythomas.py@gmail.com>
2023-01-17 19:21:15 -0600

    Initial commit

--- app.py
+++ app.py
@@ -6,14 +6,21 @@ app = Flask(__name__)
 Bootstrap(app)
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
 #
-# @app.route("/about")
-# def about():
-#     return render_template("about.html")
+# @app.route("/portfolio")
+# def portfolio():
+#     return render_template("portfolio.html")
+#
+# @app.route("/contact")
+# def contact():
+#     return render_template("contact-updated.html")
+
+app.route("/")
+def home():
+    return render_template("base.html")
 
 
 if __name__ == '__main__':
